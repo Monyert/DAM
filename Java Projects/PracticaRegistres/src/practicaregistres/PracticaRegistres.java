bd6e9f8e0d5c42cda4b59abb7c62546210c2b5a0
monyert <monyert@monyerti5>
2018-03-06 14:32:56 +0100

    Projectes curs DAM

--- Java Projects/PracticaRegistres/src/practicaregistres/PracticaRegistres.java
+++ Java Projects/PracticaRegistres/src/practicaregistres/PracticaRegistres.java
@@ -5,87 +5,68 @@
  */
 package practicaregistres;
 
+import java.util.Arrays;
+
 /**
  *
  * @author monyert
  */
+class Vivenda {
+
+    double m2;
+    int q_hab;
+    double preu;
+    NomComplet nm = new NomComplet();
+    String nif;
+}
+
+class NomComplet {
+
+    String nom;
+    String cognom1;
+    String cognom2;
+}
+
 public class PracticaRegistres {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      Vivenda v=construirVivenda();
-        System.out.println(v.m2);
+        final int TAM1 = 6;   //Nombre de escaleres
+        final int TAM2 = 8;   //Nombre de plantes 
+        final int TAM3 = 5;   //nombre de portes
+        int Edifici[][][] = new int[TAM1][TAM2][TAM3];
+        int escalera;
+        int planta;
+        int porta;
+        // Edifici [][][] edf = new Edifici [TAM1][TAM2][TAM3];
+
+        // Iniciar matriu a 0
+        for (escalera = 0; escalera < Edifici.length; escalera++) {
+            for (planta = 0; planta < Edifici[escalera].length; planta++) {
+                for (porta = 0; porta < Edifici[escalera][planta].length; porta++) {
+                   
+                }
+                 System.out.print(Edifici[planta]);
+            }
+            System.out.print(Edifici[escalera]);
+        }
         
-    }
-    
-//b)
-
-    /*
-    public static void construirVivenda() {
-
-        Utils.Utilitats.leerEnteroG("Disme el numero de l'Edifici");
-        Utils.Utilitats.leerEnteroG("Disme el numero de l'Escalera");
-        Utils.Utilitats.leerEnteroG("Disme el numero de la Planta");
-        Utils.Utilitats.leerEnteroG("Disme el numero de la Vivenda");
-        Utils.Utilitats.leerRealG("Disme els m2 de la Vivenda");
-        Utils.Utilitats.leerEnteroG("Disme el numero d'Habitacions");
-        Utils.Utilitats.leerRealG("Quin preu te la Vivenda?");
-        Utils.Utilitats.leerTextoG("Nom del propetari");
-        Utils.Utilitats.leerTextoG("Primer Cognom del propetari");
-        Utils.Utilitats.leerTextoG("Segon Cognom del propetari");
-    }
-     */
     
 
+    }
+
+    // Vivenda v = construirVivenda();
+    // System.out.println (v.m2);
     public static Vivenda construirVivenda() {
         Vivenda v = new Vivenda();
         v.m2 = Utils.Utilitats.leerRealG("Disme els m2 de la Vivenda");
         v.q_hab = Utils.Utilitats.leerEnteroG("Disme el numero d'Habitacions");
         v.preu = Utils.Utilitats.leerRealG("Quin preu te la Vivenda?");
+        v.nm.nom = Utils.Utilitats.leerTextoG("Nom del propetari");
+        v.nm.cognom1 = Utils.Utilitats.leerTextoG("Primer Cognom del propetari");
+        v.nm.cognom2 = Utils.Utilitats.leerTextoG("Segon Cognom del propetari");
         return v;
     }
-
-}
-
-
-/*    final int TAM1 = 1;
-      final int TAM2 = 1;
-      final int TAM3 = 1;
-      Edifici [][][] edf = new Edifici [TAM1][TAM2][TAM3];
- */
-
-class Edifici {
-
-    int num;
-    Escaleres esc = new Escaleres();
-}
-
-class Escaleres {
-
-    char escalera;
-    Planta plnt = new Planta();
-}
-
-class Planta {
-
-    int num;
-    Vivenda viv = new Vivenda();
-}
-
-class Vivenda {     //a)
-
-    double m2;
-    int q_hab;
-    double preu;
-    NomComplet nm = new NomComplet();
-    String[] nif;
-}
-
-class NomComplet {
-
-    String[] nom;
-    String[] cognom1;
-    String[] cognom2;
 }
