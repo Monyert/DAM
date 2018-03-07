b920283229c0ef6b02fcd5f47b34ee0160c2ca34
monyert <monyert@monyerti5>
2018-03-07 20:28:21 +0100

    Projectes curs DAM

--- Java Projects/PracticaRegistres/src/practicaregistres/PracticaRegistres.java
+++ Java Projects/PracticaRegistres/src/practicaregistres/PracticaRegistres.java
@@ -5,7 +5,7 @@
  */
 package practicaregistres;
 
-import java.util.Arrays;
+
 
 /**
  *
@@ -36,29 +36,18 @@ public class PracticaRegistres {
         final int TAM1 = 6;   //Nombre de escaleres
         final int TAM2 = 8;   //Nombre de plantes 
         final int TAM3 = 5;   //nombre de portes
-        int Edifici[][][] = new int[TAM1][TAM2][TAM3];
-        int escalera;
-        int planta;
-        int porta;
-        // Edifici [][][] edf = new Edifici [TAM1][TAM2][TAM3];
-
-        // Iniciar matriu a 0
-        for (escalera = 0; escalera < Edifici.length; escalera++) {
-            for (planta = 0; planta < Edifici[escalera].length; planta++) {
-                for (porta = 0; porta < Edifici[escalera][planta].length; porta++) {
-                   Edifici[escalera][planta][porta] = 0;
-                }
-                 
-            }
-            
-        }
+        Vivenda Edificis[][][] = new Vivenda[TAM1][TAM2][TAM3];
+        int escalera=0;
+        int planta=0;
+        int porta=0;
+        int contador=1;
         
-    
 
+        
+        comprovar(escalera, planta, porta, contador, contador, contador, contador);
     }
 
-    // Vivenda v = construirVivenda();
-    // System.out.println (v.m2);
+    
     public static Vivenda construirVivenda() {
         Vivenda v = new Vivenda();
         v.m2 = Utils.Utilitats.leerRealG("Disme els m2 de la Vivenda");
@@ -69,4 +58,57 @@ public class PracticaRegistres {
         v.nm.cognom2 = Utils.Utilitats.leerTextoG("Segon Cognom del propetari");
         return v;
     }
+    
+    public static void comprovar(int escalera,int planta,int porta,int contador,int ubicacio1,int ubicacio2,int ubicacio3){
+        //COMPROVACIO DEL CONTINGUT DE LA MATRIU
+        final int TAM1 = 6;   //Nombre de escaleres
+        final int TAM2 = 8;   //Nombre de plantes 
+        final int TAM3 = 5;   //nombre de portes
+        Vivenda Edificis[][][] = new Vivenda[TAM1][TAM2][TAM3];
+        
+        // Iniciar matriu 
+        for (escalera = 0; escalera < Edificis.length; escalera++) {
+            for (planta = 0; planta < Edificis[escalera].length; planta++) {
+                for (porta = 0; porta < Edificis[escalera][planta].length; porta++) {
+                    Edificis[escalera][planta][porta] = new Vivenda();
+                }
+
+            }
+
+        }
+        
+        for (escalera = 0; escalera < Edificis.length; escalera++) {
+            for (planta = 0; planta < Edificis[escalera].length; planta++) {
+                for (porta = 0; porta < Edificis[escalera][planta].length; porta++) {
+                    System.out.println(Edificis[escalera][planta][porta]+" es el "+ contador);
+                     contador++;
+                }
+            }
+           
+        }
+        
+        do{
+            ubicacio1=Utils.Utilitats.leerEnteroG("Disme la escala: ");
+        } while ((ubicacio1 < 1) || (ubicacio1 > 6));
+        do{
+            ubicacio2=Utils.Utilitats.leerEnteroG("Disme la planta: ");
+        } while ((ubicacio2 < 1) || (ubicacio2 > 8));
+        do{
+            ubicacio3=Utils.Utilitats.leerEnteroG("Disme la porta: ");
+        } while ((ubicacio3 < 1) || (ubicacio3 > 5));
+        
+        for (escalera = 0; escalera < Edificis.length; escalera++) {
+            for (planta = 0; planta < Edificis[escalera].length; planta++) {
+                for (porta = 0; porta < Edificis[escalera][planta].length; porta++) {
+                    if ((ubicacio1-1 == escalera) && (ubicacio2-1 == planta) && (ubicacio3-1 == porta)){
+                    Edificis[escalera][planta][porta] = construirVivenda();
+                    }
+                }
+            }
+        }
+        
+        System.out.println(Edificis[1][1][1]);
+        
+    }
 }
+
