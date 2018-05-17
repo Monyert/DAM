acbb7e5776c078a22100fba9e60a04a1fc90bab2
monyert <monyert@monyerti5>
2018-05-17 22:53:41 +0200

    Projectes curs DAM

--- Java Projects/Fitxers_Text/src/fitxers_text/Fitxers_Text.java
+++ Java Projects/Fitxers_Text/src/fitxers_text/Fitxers_Text.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,6 +32,7 @@ public class Fitxers_Text {
             System.out.println("2. Llegir fitxer Alumne");
             System.out.println("3. LLegir fitxer Plantilla");
             System.out.println("4. Corregir Exam: ");
+            System.out.println("5. Corregir Tots els fitxers");
             menu = Utilitats.leerEnteroC("Opcio Menu: ");
 
             switch (menu) {
@@ -55,15 +57,23 @@ public class Fitxers_Text {
                 }
                 break;
                 case 4: {
-                try {
-                    corregir();
-                } catch (IOException ex) {
-                    Logger.getLogger(Fitxers_Text.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                    try {
+                        corregirun();
+                    } catch (IOException ex) {
+                        Logger.getLogger(Fitxers_Text.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
                 break;
+                case 5: {
+                    try {
+                        //corregirtots
+                        corregirtots(Utilitats.leerTextoG("Disme la ruta del directori: "));
+                    } catch (IOException ex) {
+                        Logger.getLogger(Fitxers_Text.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
-        } while (menu > 0 && menu < 5);
+        } while (menu > 0 && menu < 6);
 
     }
 
@@ -119,44 +129,112 @@ public class Fitxers_Text {
         br.close();
         fr.close();
     }
-    
-    public static void corregir() throws IOException {
-        FileReader fr = null;
-        try {
-            fr = new FileReader(Utilitats.leerTextoG("Disme la ruta del ficher Respostes: "));
 
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Fitxers_Text.class.getName()).log(Level.SEVERE, null, ex);
+    public static void corregirun() throws IOException {
+
+        File f = new File(Utilitats.leerTextoG("Disme la ruta del corrector: "));
+        File f2 = new File(Utilitats.leerTextoG("Disme la ruta del Alumne: "));
+
+        FileReader fr = new FileReader(f);
+        FileReader fr2 = new FileReader(f2);
+
+        int lletra;
+        int lletra2;
+        int E = 0, B = 0, F = 0;
+        System.out.println("*** Comprovacio Visual ***");
+        while ((lletra = fr.read()) != -1) {
+            while ((lletra2 = fr2.read()) != -1) {
+
+                System.out.println("");
+                if ((char) lletra == (char) lletra2) {
+                    E++;
+                } else if ((char) lletra2 == '-') {
+                    B++;
+                } else {
+                    F++;
+                }
+
+                System.out.print((char) lletra + " " + (char) lletra2);
+                break;
+            }
         }
-        BufferedReader br = new BufferedReader(fr);
-        String s = "";
-        while (br.ready()) {
-            s = br.readLine();
-        }
-        br.close();
+        double nota;
+        nota = (E * 0.5) - (F * 0.25);
+        System.out.println("");
+        System.out.println("Notes de " + f2.getName() + "\b\b\b\b. La nota és de: " + nota + ". E,B,F --> [" + E + "," + B + "," + F + "]");
+        System.out.println("");
         fr.close();
-        
-        FileReader fr2 = null;
-        try {
-            fr2 = new FileReader(Utilitats.leerTextoG("Disme la ruta del ficher Alumne: "));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Fitxers_Text.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        BufferedReader br2 = new BufferedReader(fr2);
-        String s2 = "";
-        while (br2.ready()) {
-            s2 = br2.readLine();
-        }
-        boolean es;
-        es = s.equalsIgnoreCase(s2);
-        if (es == true){
-            Utilitats.muestraMensajeG("Totes les respostes correctes");
-        }else{
-            Utilitats.muestraMensajeG("Algo tens mal");
-        }
-        br2.close();
         fr2.close();
+    }
+
+    public static void corregirtots(String ruta) throws IOException {
+        File d = new File(ruta);
+
+        System.out.println("És un directori: " + d.isDirectory());
+        String[] elsFitxers = d.list();
+        System.out.println("Llistat de Fitxers de: " + d.getAbsolutePath());
+
+        //File f = new File(Utilitats.leerTextoG("Disme la ruta del Directori: "));
+        //Primer Mostrem Tota la LLista
+        
+        if (d.isDirectory() == true) {
+            for (String f : elsFitxers) {
+                System.out.println("\t" + f);
+            }
+            System.out.println("");
+        } else {
+            System.out.println("No es un directori.");
+            System.out.println("");
+            System.exit(0);
+            
+        }
+        
+        
+        FileReader fr = new FileReader(Utilitats.leerTextoG("Disme quin de tots es el corrector: "));
+        File f2;
+        FileReader fr2 = null;
+        if (d.isDirectory() == true) {
+            for (String s : elsFitxers) {
+                
+                //System.out.println("\t" + s);
+                f2 = new File(d.getAbsolutePath() +FileSystems.getDefault().getSeparator()+ s);
+                
+                fr2 = new FileReader(f2);
+
+                int lletra;
+                int lletra2;
+                int E = 0, B = 0, F = 0;
+                while ((lletra = fr.read()) != -1) {
+                    while ((lletra2 = fr2.read()) != -1) {
+
+                        if ((char) lletra == (char) lletra2) {
+                            E++;
+                        } else if ((char) lletra2 == '-') {
+                            B++;
+                        } else {
+                            F++;
+                        }
+
+                       // System.out.print((char) lletra + " " + (char) lletra2);
+                       lletra = 0;
+                       lletra2 = 0;
+                        break;
+                    }
+                }
+                double nota;
+                nota = (E * 0.5) - (F * 0.25);
+                System.out.println("Notes de " + f2.getName() + "\b\b\b\b. La nota és de: " + nota + ". E,B,F --> [" + E + "," + B + "," + F + "]");
+                
+
+            }
+            System.out.println("");
+            fr.close();
+            fr2.close();
+        } else {
+            System.out.println("No es un directori.");
+            System.out.println("");
+        }
         
     }
-    
+
 }
